6583ce2c7a1399940408f2255495613d88aa6099
Jonny Alejandro <jonny.cano@valorganado.com>
2021-06-12 00:15:20 -0500

    js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
 'use strict'
 
-console.log("hola mundo");
+function loginSubmit() {
+    user = document.getElementById('emailLogin');
+    pass = document.getElementById('passLogin');
+    typeUser = document.getElementById('typeUser');
+    if (user) {
+        console.log(user);
+    }
+}
