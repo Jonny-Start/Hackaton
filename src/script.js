d328e63078b803dab588adcefee9b07b333e5a34
Jonny Alejandro <jonny.cano@valorganado.com>
2021-06-12 01:35:02 -0500

    Nuevas vistas y funcionalidad OK}

--- src/script.js
+++ src/script.js
@@ -1,32 +1,33 @@
 'use strict'
 
-const users = [
-  {
-    user: 'daniel7byte@gmail.com',
-    pass: 'secret',
-    typeUser: 'client'
-  },
-  {
-    user: 'd.mayama@utp.edu.co',
-    pass: 'secret',
-    typeUser: 'driver'
-  }
+const users = [{
+        user: 'pruebaclient@gmail.com',
+        pass: 'secret',
+        typeUser: '1'
+    },
+    {
+        user: 'pruebadriver@gmail.com',
+        pass: 'secret',
+        typeUser: '2'
+    }
 ]
 
-function checkUser (userName, pass, typeUser) {
-  return users.some(user => {
-    return user.user === userName && user.pass === pass && user.typeUser === typeUser
-  })
+function checkUser(userName, pass, typeUser) {
+    return users.some(user => {
+        return user.user === userName && user.pass === pass && user.typeUser === typeUser
+    })
 }
 
-function loginSubmit () {
-  const userName = document.getElementById('emailLogin').value
-  const pass = document.getElementById('passLogin').value
-  const typeUser = document.getElementById('typeUser').value
+function loginSubmit() {
+    const userName = document.getElementById('emailLogin').value
+    const pass = document.getElementById('passLogin').value
+    const typeUser = document.getElementById('typeUser').value
 
-  if (checkUser(userName, pass, typeUser)) {
-    window.location = 'front/manage_tokens.html'
-  } else {
-    alert('Usuario no encontrado')
-  }
+    if (checkUser(userName, pass, typeUser) && typeUser == 1) {
+        window.location = 'front/maps.html';
+    } else if (checkUser(userName, pass, typeUser) && typeUser == 2) {
+        window.location = 'front/portalConductor.html';
+    } else {
+        alert('Usuario no encontrado');
+    }
 }
